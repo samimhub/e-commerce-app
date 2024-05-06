55cc016c6cb7d653a5e223f4e16b29ff13d0741d
samim <aktarsamim529@gmail.com>
2024-05-07 02:14:45 +0530

    new commite

--- app/Components/Cards.tsx
+++ app/Components/Cards.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import { Product } from './Products';
+import Image from 'next/image';
 
 
 
@@ -8,14 +9,21 @@ interface CardsProps {
   filterItems: Product[];
 }
 
-const Cards: React.FC<CardsProps> = ({ filterItems })=>{
+const Cards: React.FC<CardsProps> = ({ filterItems }) => {
   return (
     <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 items-center justify-center gap-10 shadow-sm'>
       {
         filterItems.map((items) => (
           <div key={items.id}>
             <Link href={`/shop/${items.id}`}>
-              <img src={items.image} alt="" className='mx-auto w-full drop-shadow-2xl border rounded-sm hover:scale-105 transition-all duration-200' />
+              <Image
+                src={items.image}
+                alt=""
+                className='mx-auto w-full drop-shadow-2xl border rounded-sm hover:scale-105 transition-all duration-200'
+                layout="responsive"
+                width={100}
+                height={100}
+              />
             </Link>
             <div className='mt-4 px-4'>
               <h4 className='text-base font-semibold mb-2'>{items.name}</h4>
--- app/Components/Category.tsx
+++ app/Components/Category.tsx
@@ -1,70 +1,107 @@
 "use client"
 
+import Image from "next/image";
 import Link from "next/link";
 import Marquee from "react-fast-marquee";
 import { MdArrowForwardIos } from "react-icons/md";
 
-export const brandLogo=[
+export const brandLogo = [
     {
-        id:0,
-        img:"cloth.png"
+        id: 0,
+        img: "cloth.png"
     },
     {
-        id:1,
-        img:"shoe.png"
+        id: 1,
+        img: "shoe.png"
     },
     {
-        id:2,
-        img:"technology.png"
+        id: 2,
+        img: "technology.png"
     },
     {
-        id:3,
-        img:"store.png"
+        id: 3,
+        img: "store.png"
     },
     {
-        id:4,
-        img:"cloth.png"
+        id: 4,
+        img: "cloth.png"
     },
     {
-        id:5,
-        img:"shoe.png"
+        id: 5,
+        img: "shoe.png"
     },
 ]
 
 
 function Category() {
-  return (
-    <div className="max-w-screen-2xl mx-auto container lg:px-28 px-4 py-14">
-       {/*Brand Logo */}
-        <div className="text-center font-semibold uppercase tracking-[0.2rem] lg:tracking-[1rem] text-blue-500 pb-5"> Collection of Brand</div>
-        <Marquee speed={120} className="flex items-center justify-start gap-4 py-5">
-        {
-            brandLogo.map(({id,img})=>(
-                <div key={id}>
-                    <img className="h-[120px] w-[120] lg:h-[180px] lg:w-[180]" src={img} alt="brand"/>
+    return (
+        <div className="max-w-screen-2xl mx-auto container lg:px-28 px-4 py-14">
+            {/*Brand Logo */}
+            <div className="text-center font-semibold uppercase tracking-[0.2rem] lg:tracking-[1rem] text-blue-500 pb-5"> Collection of Brand</div>
+            <Marquee speed={120} className="flex items-center justify-start gap-4 py-5">
+                {
+                    brandLogo.map(({ id, img }) => (
+                        <div key={id}>
+                            <Image
+                                src={img}
+                                alt="brand"
+                                layout="fill"
+                                objectFit="cover"
+                                className="h-[120px] w-[120] lg:h-[180px] lg:w-[180]"
+                            />
+                        </div>
+                    ))
+                }
+            </Marquee>
+            <div className="mt-6 flex flex-col  md:flex-row items-center gap-4">
+                <button className="w-[40%] flex font-semibold md:-rotate-90 uppercase tracking-[0.2rem] md:tracking-[0.5rem] text-blue-500 md:text-white pb-5 md:bg-gray-600 p-2 rounded-sm">
+                    Explpor now
+                    <MdArrowForwardIos className="text-2xl mt-1" />
+                </button>
+                <div>
+                    <Link href="/">
+                        <div className="relative w-full h-full">
+                            <Image
+                                src="/fashion (2).jpg"
+                                alt="fashion"
+                                layout="fill"
+                                objectFit="cover"
+                                className="w-full hover:scale-105 transition-all duration-200"
+                            />
+                        </div>
+                    </Link>
                 </div>
-            ))
-        }
-        </Marquee>
-        <div className="mt-6 flex flex-col  md:flex-row items-center gap-4">
-            <button className="w-[40%] flex font-semibold md:-rotate-90 uppercase tracking-[0.2rem] md:tracking-[0.5rem] text-blue-500 md:text-white pb-5 md:bg-gray-600 p-2 rounded-sm">
-                Explpor now 
-                <MdArrowForwardIos className="text-2xl mt-1"/>
-            </button>
-            <div>
-                <Link href="/"><img src="/fashion (2).jpg" alt="fashion" className="w-full hover:scale-105 transition-all duration-200"/></Link>
-            </div>
-            <div className="md:w-1/2">
-                <div className="grid grid-cols-2 gap-4">
-                    <Link href="/"><img src="fashion.jpg" alt="fashion" className="hover:scale-105 transition-all duration-200"/></Link>
-                    <Link href="/"><img src="fashion.jpeg" alt="fashion"className="hover:scale-105 transition-all duration-200"/></Link>
+                <div className="md:w-1/2">
+                    <div className="grid grid-cols-2 gap-4">
+                        <Link href="/">
+                            <div className="relative w-full h-full">
+                                <Image
+                                    src="/fashion.jpg"
+                                    alt="fashion"
+                                    layout="fill"
+                                    objectFit="cover"
+                                    className="hover:scale-105 transition-all duration-200"
+                                />
+                            </div>
+                        </Link>
+                        <Link href="/">
+                            <div className="relative w-full h-full">
+                                <Image
+                                    src="/fashion.jpeg"
+                                    alt="fashion"
+                                    layout="fill"
+                                    objectFit="cover"
+                                    className="hover:scale-105 transition-all duration-200"
+                                />
+                            </div>
+                        </Link>
+                    </div>
                 </div>
-            
+
             </div>
         </div>
-    </div>
 
-  )
+    )
 }
 
 export default Category
